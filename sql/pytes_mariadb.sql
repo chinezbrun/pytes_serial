a717c706a4072c814e943d605be2917c84c162af
Dan Popescu <35501848+chinezbrun@users.noreply.github.com>
2023-01-23 20:42:30 +0200

    Update pytes_mariadb.sql
    allow non critical data to be Null

--- sql/pytes_mariadb.sql
+++ sql/pytes_mariadb.sql
@@ -2,10 +2,10 @@
 -- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Gazdă: localhost
--- Timp de generare: oct. 12, 2022 la 09:35 PM
--- Versiune server: 10.3.32-MariaDB
--- Versiune PHP: 7.4.28
+-- Host: localhost
+-- Generation Time: Jan 23, 2023 at 08:38 PM
+-- Server version: 10.3.32-MariaDB
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Bază de date: `pytes`
+-- Database: `pytes`
 --
 CREATE DATABASE IF NOT EXISTS `pytes` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `pytes`;
@@ -27,11 +27,11 @@ USE `pytes`;
 -- --------------------------------------------------------
 
 --
--- Structură tabel pentru tabel `pwr_data`
+-- Table structure for table `pwr_data`
 --
 
-CREATE TABLE `pwr_data` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `pwr_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `record_time` datetime NOT NULL DEFAULT current_timestamp(),
   `power` int(11) NOT NULL,
   `voltage` float NOT NULL,
@@ -39,18 +39,16 @@ CREATE TABLE `pwr_data` (
   `temperature` decimal(11,0) NOT NULL,
   `soc` int(11) NOT NULL,
   `basic_st` varchar(11) NOT NULL,
-  `volt_st` varchar(11) NOT NULL,
-  `current_st` varchar(11) NOT NULL,
-  `temp_st` varchar(11) NOT NULL,
-  `coul_st` varchar(11) NOT NULL,
-  `soh_st` varchar(11) NOT NULL,
-  `heater_st` varchar(11) NOT NULL,
-  `bat_events` int(11) NOT NULL,
-  `power_events` int(11) NOT NULL,
-  `sys_events` int(11) NOT NULL,
-   CONSTRAINT id PRIMARY KEY (id)
-  
-
+  `volt_st` varchar(11) DEFAULT NULL,
+  `current_st` varchar(11) DEFAULT NULL,
+  `temp_st` varchar(11) DEFAULT NULL,
+  `coul_st` varchar(11) DEFAULT NULL,
+  `soh_st` varchar(11) DEFAULT NULL,
+  `heater_st` varchar(11) DEFAULT NULL,
+  `bat_events` int(11) DEFAULT NULL,
+  `power_events` int(11) DEFAULT NULL,
+  `sys_events` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
 
