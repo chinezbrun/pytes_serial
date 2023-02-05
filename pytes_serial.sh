2414cfcfb90520a207bafb375cd5fd774bfde6e9
Dan Popescu <35501848+chinezbrun@users.noreply.github.com>
2023-02-05 08:41:09 +0200

    Update pytes_serial.sh
    adapted for better usage in autostart scripts

--- pytes_serial.sh
+++ pytes_serial.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# navigate in the directory where python script is placed
+cd /home/pi/Documents/pytes
+# open the script in a separate terminal 
 lxterminal -e python3 pytes_serial.py
+# if not just run it 
+#python3 pytes_serial.py
